84ba8db50a6a8a746357af6fb3003066a2304394
Christian Stroh <christian.stroh@hjp-consulting.com>
2015-09-07 11:14:20 +0200

    Moved instantiating of driver connector from PersoSimGui to de.persosim.rcp.Activator

--- de.persosim.rcp/src/de/persosim/rcp/Activator.java
+++ de.persosim.rcp/src/de/persosim/rcp/Activator.java
@@ -1,18 +1,74 @@
 package de.persosim.rcp;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import de.persosim.simulator.PersoSim;
+import de.persosim.simulator.perso.Personalization;
+import de.persosim.simulator.perso.PersonalizationFactory;
+import de.persosim.simulator.ui.parts.PersoSimGuiMain;
 
 public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
 		de.persosim.simulator.Activator.getDefault().enableService();
+		getSimAndConnectToNativeDriver();
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		de.persosim.simulator.Activator.getDefault().disableService();
+	}
+	
+	/**
+	 * This method handles the connection to the simulator. Its primary task is
+	 * to ensure the simulator is up and running when a connection is
+	 * initialized. If the simulator is not found to be running a default FIXME JGE the implementation is NOT conditional!
+	 * personalization is loaded.
+	 */
+	private void getSimAndConnectToNativeDriver() {
+		    de.persosim.simulator.Activator persoSimActivator = de.persosim.simulator.Activator.getDefault();
+		    PersoSim sim = persoSimActivator.getSim();
+		    try {
+		    	sim.startSimulator();
+				sim.loadPersonalization(getDefaultPersonalization());
+			} catch (IOException e) {
+				e.printStackTrace();
+				MessageDialog.openError(null, "Error", "Failed to automatically load default personalization");
+				return;
+			}
+		    de.persosim.simulator.ui.Activator.connectToNativeDriver();
+	}
+	
+	/**
+	 * This method returns a personalization which can be used as default.
+	 * @return a default personalization
+	 * @throws IOException
+	 */
+	private Personalization getDefaultPersonalization() throws IOException {
+		Bundle plugin = Platform.getBundle(PersoSimGuiMain.DE_PERSOSIM_SIMULATOR_BUNDLE);
+		URL url = plugin.getEntry (PersoSimGuiMain.PERSO_PATH);
+		URL resolvedUrl;
+		
+		resolvedUrl = FileLocator.resolve(url);
+		
+		File folder = new File(resolvedUrl.getFile());
+		String pathString = folder.getAbsolutePath() + File.separator + PersoSimGuiMain.PERSO_FILE;
+		
+		System.out.println("Loading default personalization from: " + pathString);
+		
+		Personalization personalization = (Personalization) PersonalizationFactory.unmarshal(pathString);
+		
+		return personalization;
 	}
 
 }

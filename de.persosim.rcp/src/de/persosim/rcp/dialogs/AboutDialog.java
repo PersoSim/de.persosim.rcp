725cec6d966379c86a66448d8a60e556f2ecd77a
Stefan Philipp <stefan.philipp@secunet.com>
2024-06-18 13:33:43 +0200

    GT-358 Updated target platform from 4.30 to 4.31 (2024-03); Use new orbit simrel instead of deprecated drops in target platform; update from javax.inject/annotation to jakarta pendants; add owasp suppressions files to suppress invalid vulnerabilities in dependency check reports

--- de.persosim.rcp/src/de/persosim/rcp/dialogs/AboutDialog.java
+++ de.persosim.rcp/src/de/persosim/rcp/dialogs/AboutDialog.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.eclipse.core.runtime.IProduct;
 import org.eclipse.core.runtime.Platform;

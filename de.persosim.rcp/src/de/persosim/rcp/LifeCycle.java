7e061af433d714b91f8f168215a69ebf1f643cff
Martin Boonk <martin.boonk@secunet.com>
2025-05-21 09:57:05 +0200

    Add @SuppressWarnings for use of IEventLoopAdvisor

--- de.persosim.rcp/src/de/persosim/rcp/LifeCycle.java
+++ de.persosim.rcp/src/de/persosim/rcp/LifeCycle.java
@@ -7,13 +7,21 @@ import org.eclipse.swt.widgets.Display;
 import org.globaltester.logging.BasicLogger;
 import org.globaltester.logging.tags.LogLevel;
 
+/**
+ * This implements logging of event loop exceptions as described
+ * in https://stackoverflow.com/questions/48209573/custom-error-dialog-in-eclipse-e4-application
+ * 
+ * The use of the internal IEventLoopAdvisor seems to be the only valid solution at this time.
+ */
 public class LifeCycle {
 
+	@SuppressWarnings("restriction")
 	@PostContextCreate
 	public void postContextCreate(IEclipseContext context) {
 		context.set(IEventLoopAdvisor.class, new EventLoopAdvisor(context));
 	}
 
+	@SuppressWarnings("restriction")
 	class EventLoopAdvisor implements IEventLoopAdvisor {
 		EventLoopAdvisor(IEclipseContext appContext) {
 			super();
